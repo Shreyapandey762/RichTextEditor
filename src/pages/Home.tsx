8134c53ff6be8d44b1dd2774b90efbbf43678178
Shreyapandey762 <shreya2002pandey@gmail.com>
2025-02-05 15:12:02 +0530

    layout improved

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,23 +6,18 @@ import RichTextEditor from "../components/RichTextEditor";
 
 const Home: React.FC = () => {
     return (
-        <Box sx={{ padding: 3, height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-            <Grid container spacing={3} sx={{ maxWidth: "90vw" }}>
-                {/* Counter Component - Top Left */}
+        <Box sx={{ padding: 3, height: "100vh", display: "flex", justifyContent: "center", alignItems: "center",width:'100vw' }}>
+            <Grid container spacing={3} sx={{ width:'100%' }}>
                 <Grid item xs={12} sm={6}>
                     <Paper elevation={3} sx={{ padding: 2, height: "100%" }}>
                         <Counter />
                     </Paper>
                 </Grid>
-
-                {/* Rich Text Editor - Top Right */}
                 <Grid item xs={12} sm={6}>
                     <Paper elevation={3} sx={{ padding: 2, height: "100%" }}>
                         <RichTextEditor />
                     </Paper>
                 </Grid>
-
-                {/* User Data Form - Full Width Below */}
                 <Grid item xs={12}>
                     <Paper elevation={3} sx={{ padding: 2 }}>
                         <UserDataForm />
